d7106aa4d39ef8104bc6cf7845479c794717d243
JacobSletten <slettenj2628@my.uwstout.edu>
2024-04-04 10:53:55 -0500

    stuff and things

--- app/src/main/java/com/example/spotme/ui/SummaryScreen.kt
+++ app/src/main/java/com/example/spotme/ui/SummaryScreen.kt
@@ -60,12 +60,45 @@ fun SummaryScreen(
                     .wrapContentHeight()
                     .padding(dimensionResource(R.dimen.padding_small))
             ) {
-                Column(modifier.padding(dimensionResource(R.dimen.padding_small))) {
-                    Text("Balance:")
+                Row(modifier.padding(dimensionResource(R.dimen.padding_small))) {
+                    Text("Overall Balance:")
                     Text("$" + totalBalance.totalBalance.toString(), modifier)
                 }
             }
+            Row(){
+                Card(
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.tertiaryContainer
+                    ),
+                    modifier = modifier
+                        .wrapContentHeight()
+                        .padding(dimensionResource(R.dimen.padding_small))
+                        .weight(1f)
+                ) {
+                    Column(modifier.padding(dimensionResource(R.dimen.padding_small))) {
+                        Text("Largest Debtor:")
+                        Text("name")
+                        Text("$" )
+                    }
+                }
+                Card(
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.tertiaryContainer
+                    ),
+                    modifier = modifier
+                        .wrapContentHeight()
+                        .padding(dimensionResource(R.dimen.padding_small))
+                        .weight(1f)
+                ) {
+                    Column(modifier.padding(dimensionResource(R.dimen.padding_small))) {
+                        Text("Main Creditor:")
+                        Text("name")
+                        Text("$" )
+                    }
+                }
+            }
         }
+
         //Basic Nav Buttons
         Row( //NavButtons
             verticalAlignment = Alignment.CenterVertically,
