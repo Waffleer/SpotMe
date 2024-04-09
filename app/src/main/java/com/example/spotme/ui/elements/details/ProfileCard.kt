abb01ab996efa1ae596f843cac820962768f4099
Digi <aus343@gmail.com>
2024-04-09 10:38:54 -0500

    Jetty Improved Profile Cards

--- app/src/main/java/com/example/spotme/ui/elements/details/ProfileCard.kt
+++ app/src/main/java/com/example/spotme/ui/elements/details/ProfileCard.kt
@@ -1,30 +1,33 @@
 package com.example.spotme.ui.elements.details
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.spotme.R
 import com.example.spotme.data.Profile
 import com.example.spotme.data.StaticDataSource
-import com.example.spotme.ui.elements.NavButton
+import com.example.spotme.ui.elements.DetailsNavButton
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileCard(
@@ -32,43 +35,85 @@ fun ProfileCard(
     onClicked: (Profile) -> Unit,
     onPlusClicked: (Profile) -> Unit,
     modifier: Modifier = Modifier
-    ) {
+) {
+    var total: Double = 0.0
 
-    Card(modifier = modifier
-        .padding(12.dp)
-        .clip(RoundedCornerShape(10.dp))
-        .border(BorderStroke(2.dp, SolidColor(Color.Black))),
+    profile.debts.forEach { debt -> //Totals all of the transactions for every debt
+        debt.transactions.forEach { trans ->
+            if (trans.canceled == false) {
+                total += trans.amount
+            }
+        }
+    }
 
+    @Composable
+    fun Modifier.totalColor(total: Double): Modifier {
+        val color = if (total > 0) Color.Green
+        else if (total == 0.0) Color.LightGray
+        else Color.Red
+        return this.then(Modifier.drawWithContent {
+            drawContent()
+            drawRect(color = color, alpha = 0.2f)
+        })
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 5.dp, end = 5.dp),
+        contentAlignment = Alignment.Center
     ) {
-        Row {
-            Card(
-                onClick = {onClicked(profile)}
-            ) {
+        Card(
+            modifier = modifier
+                .padding(12.dp)
+                .clip(RoundedCornerShape(15.dp))
+
+        ) {
+            Row {
+                Card(
+                    onClick = { onClicked(profile) }
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth(0.6f) //DO NOT TOUCH OR IT WILL BREAK FOR THE LOVE OF GOD
+                            .height(100.dp)
+                    )
+                    {
+                        Text(
+                            textAlign = TextAlign.Start,
+                            text = profile.name,
+                            modifier = Modifier
+                                .padding(start = 12.dp, top = 8.dp),
+                            fontSize = 30.sp,
+                            fontWeight = FontWeight.Bold
+
+                        )
+                        Divider(
+                            color = Color.Black,
+                            thickness = 2.dp,
+                            modifier = Modifier
+                                .padding(start = 12.dp, end = 12.dp, top = 4.dp, bottom = 4.dp)
+                        )
+                        Text(
+                            textAlign = TextAlign.Start,
+                            text = "$$total",
+                            modifier = Modifier
+                                .padding(start = 12.dp, bottom = 4.dp)
+                                .totalColor(total),
+                        )
+                    }
+                }
                 Column(
-                    Modifier.width(160.dp)
-                )
-                {
-                    Text(
-                        textAlign = TextAlign.Start,
-                        text = profile.name,
-                        modifier = Modifier
-                            .padding(start = 12.dp, top = 4.dp),
-                    )
-                    Divider(
-                        color = Color.Black,
-                        thickness = 2.dp,
-                        modifier = Modifier
-                            .padding(start = 12.dp, end = 12.dp, top = 4.dp, bottom = 4.dp)
-                    )
-                    Text(
-                        textAlign = TextAlign.Start,
-                        text = "$${profile.amount}",
-                        modifier = Modifier
-                            .padding(start = 12.dp, bottom = 4.dp),
+                    modifier = Modifier
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    DetailsNavButton(
+                        labelResourceId = R.string.plus_button,
+                        onClick = { onPlusClicked(profile) }
                     )
                 }
             }
-            NavButton(labelResourceId = R.string.plus_button, onClick = { onPlusClicked(profile) })
         }
     }
 }
@@ -76,6 +121,6 @@ fun ProfileCard(
 
 @Preview
 @Composable
-fun PreviewProfileCard(){
+fun PreviewProfileCard() {
     ProfileCard(profile = StaticDataSource.profiles[0], {}, {})
 }
