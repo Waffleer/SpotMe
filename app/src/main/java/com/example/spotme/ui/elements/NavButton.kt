644a5c36a67fa0e42fc3482882c6f33343790642
Waffleer <84477153+Waffleer@users.noreply.github.com>
2024-04-23 10:24:25 -0500

    Fixed Some Imports

--- app/src/main/java/com/example/spotme/ui/elements/NavButton.kt
+++ app/src/main/java/com/example/spotme/ui/elements/NavButton.kt
@@ -7,7 +7,11 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
